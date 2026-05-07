f749e0885c4e87ba91e2a0adaa2645d39805abf9
Anish Roy <aroy262692@gmail.com>
2026-05-07 14:37:35 +0530

    add: TrustedBySection component and integrate it into ExportPage

--- components/export/index.ts
+++ components/export/index.ts
@@ -4,3 +4,4 @@ export { default as SingaporeSection } from "./Singapore.export";
 export { default as GlobalGoalsSection } from "./GlobalGoals.export";
 export { default as RoadmapSection } from "./Roadmap.export";
 export { default as PartnershipCTASection } from "./PartnershipCTA.export";
+export { default as TrustedBySection } from "./TrustedBy.export";
